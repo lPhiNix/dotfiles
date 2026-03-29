6065af4b50fb22ad87c620561aab9c79ca575faa
lphinix <pmp.phinix.dev@proton.me>
2026-03-29 11:50:58 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+set -g fish_greeting ""
+
+alias ls="lsd"
+alias ll="lsd -a"
+alias lll="lsd -la"
+alias cls="clear"
+
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 thefuck --alias | source
+
