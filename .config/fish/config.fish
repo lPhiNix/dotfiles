4e56630fdf9551ed381b54c988b696c886ce7306
lphinix <pmp.phinix.dev@proton.me>
2026-04-05 16:35:08 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,41 @@ end
 
 set -g fish_greeting ""
 
+alias d="dotfiles"
+
+alias p="sudo pacman"
+alias y="sudo yay"
+
+alias v="nvim"
+alias vim="nvim"
+
 alias ls="lsd"
-alias ll="lsd -a"
-alias lll="lsd -la"
+alias la="lsd -a"
+alias ll="lsd -l"
+alias lla="lsd -la"
+
+alias g="git"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gpl="git pull"
+alias gl="git log --oneline --graph --decorate"
+alias gb="git branch"
+alias gcl="git clone"
+alias gr="git remote"
+alias grv="git remote -v"
+
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+
+alias top="htop"
+alias cat="bat"
+
 alias cls="clear"
+alias q="exit"
+alias shutdown="shutdown -h now"
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 thefuck --alias | source
