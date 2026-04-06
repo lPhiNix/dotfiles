30ab5106d93f753f6c5d14c48918aa03a9b7999c
lphinix <pmp.phinix.dev@proton.me>
2026-04-06 16:38:28 +0200

    Update waybar config

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#                    __
+#  _    _____ ___ __/ /  ___ _____
+# | |/|/ / _ `/ // / _ \/ _ `/ __/
+# |__,__/\_,_/\_, /_.__/\_,_/_/
+#            /___/
+#
 # Terminate already running bar instances
 pkill waybar
 
